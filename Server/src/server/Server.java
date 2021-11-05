207422d42217ba962c41481d4ff2adf1d0f064ec
TurulR3D <officialzakaigan@gmail.com>
2021-11-05 13:47:13 +0100

    Update Server.java

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -10,49 +10,53 @@ public class Server {
 	static int port = 2345;
     double rate, amount;
     int years,clients;
+    int p1score, p2score, p3score, p1lives, p2lives, p3lives;
  
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
         new Thread(() -> {
-            try {
-
-                ServerSocket server = new ServerSocket(port);
-                System.out.println("Server started at: " + new Date());
-
-                int clients = 0;
-
-                while (true) {
-
-
-                    Socket clientSocket = server.accept();
-                    clients++;
-                    System.out.println("Connection to client at: " + new Date());
-
-
-                    System.out.println("Processing client "+clients+" at "+new Date());
-
-
-                    InetAddress inetAddress = clientSocket.getInetAddress();
-                    System.out.println("Client "+clients+" has name: "+ inetAddress.getHostName()+" and address: "+inetAddress.getHostAddress());
-
-                    //New thread is made with a Player constructor, that passes client socket and server name on
-                    if (clients < 3){
-                    	new Thread(new Player(clientSocket, "multi server", clients, this)).start();
-                    }
-                    
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("Lost connection to server socket");
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.out.println("something went wrong");
-            }
-
+            Server server = new Server();
+        	server.clientThread();
         }).start();
         
-       
+
+    }
+    
+    public void clientThread() {
+    	try {
+
+            ServerSocket server = new ServerSocket(port);
+            System.out.println("Server started at: " + new Date());
+
+            int clients = 0;
+
+            while (true) {
+
+
+                Socket clientSocket = server.accept();
+                clients++;
+                System.out.println("Connection to client at: " + new Date());
+
+
+                System.out.println("Processing client "+clients+" at "+new Date());
+
+
+                InetAddress inetAddress = clientSocket.getInetAddress();
+                System.out.println("Client "+clients+" has name: "+ inetAddress.getHostName()+" and address: "+inetAddress.getHostAddress());
+
+                //New thread is made with a Player constructor, that passes client socket and server name on
+                if (clients < 3){
+                	new Thread(new Player(clientSocket, "multi server", clients, this)).start();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Lost connection to server socket");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("something went wrong");
+        }
     }
 }
